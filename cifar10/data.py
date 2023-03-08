eb22a8ea85fe62098b38fce7df572802808ad3ed
lijiang99 <bd7xzz@163.com>
2023-03-08 14:24:35 +0800

    update cifar10/data.py

--- cifar10/data.py
+++ cifar10/data.py
@@ -13,9 +13,9 @@ def load_cifar10(path, batch_size):
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
     
-    train_set = datasets.CIFAR10(root=path, train=True, download=False, transform=transform_train)
+    train_set = datasets.CIFAR10(root=path, train=True, download=True, transform=transform_train)
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=2)
-    val_set = datasets.CIFAR10(root=path, train=False, download=False, transform=transform_test)
+    val_set = datasets.CIFAR10(root=path, train=False, download=True, transform=transform_test)
     val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=2)
     
     return train_loader, val_loader
