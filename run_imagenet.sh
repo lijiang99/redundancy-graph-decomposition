ab22d5d167905ce2bd41d036dd0ba1a7cf79ebaf
lijiang99 <bd7xzz@163.com>
2023-04-07 22:11:01 +0800

    add vgg-16/19-bn

--- run_imagenet.sh
+++ run_imagenet.sh
@@ -1,16 +1,27 @@
 # !/bin/bash
-arch="resnet50"
+archs=("resnet50" "vgg16_bn" "vgg19_bn")
 pretrain_dir="./imagenet/pre-train/"
 dataset_dir="./imagenet/dataset/"
 prune_info_saved_dir="./imagenet/prune-info/"
 prune_info_log_dir="./imagenet/log/generate_prune_info/"
-thresholds=(0.7 0.65)
 
-for threshold in ${thresholds[@]}
+for arch in ${archs[@]}
 do
-	prune_info_path="./imagenet/prune-info/"$arch"-"$threshold".json"
-	if [ ! -f "$prune_info_path" ]; then
-		python generate_prune_info.py --arch $arch --pretrain-dir $pretrain_dir --dataset-dir $dataset_dir --saved-dir $prune_info_saved_dir --log-dir $prune_info_log_dir --threshold $threshold
-	fi
-	python prune_imagenet.py --arch $arch --threshold $threshold
+	case $arch in
+		"vgg"*)
+			thresholds=(0.7 0.75)
+			;;
+		"resnet50")
+			thresholds=(0.65 0.7)
+			;;
+		*)
+	esac
+	for threshold in ${thresholds[@]}
+	do
+		prune_info_path="./imagenet/prune-info/"$arch"-"$threshold".json"
+		if [ ! -f "$prune_info_path" ]; then
+			python generate_prune_info.py --arch $arch --pretrain-dir $pretrain_dir --dataset-dir $dataset_dir --saved-dir $prune_info_saved_dir --log-dir $prune_info_log_dir --threshold $threshold
+		fi
+		python prune_imagenet.py --arch $arch --threshold $threshold
+	done
 done
