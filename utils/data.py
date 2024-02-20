1b7259b10fe242e4744624cba4e3f03be8314f68
lijiang99 <bd7xzz@163.com>
2024-02-20 20:49:14 +0800

    add load_cifar100

--- utils/data.py
+++ utils/data.py
@@ -1,3 +1,4 @@
+import os
 from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
 
@@ -6,16 +7,56 @@ def load_cifar10(path, batch_size):
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]),
     ])
     transform_test = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]),
     ])
     
     train_set = datasets.CIFAR10(root=path, train=True, download=True, transform=transform_train)
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=2)
     val_set = datasets.CIFAR10(root=path, train=False, download=True, transform=transform_test)
     val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=2)
+    
+    return train_loader, val_loader
+
+def load_cifar100(path, batch_size):
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]),
+    ])
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]),
+    ])
+    
+    train_set = datasets.CIFAR100(root=path, train=True, download=True, transform=transform_train)
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=2)
+    val_set = datasets.CIFAR100(root=path, train=False, download=True, transform=transform_test)
+    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=2)
+    
+    return train_loader, val_loader
+
+def load_imagenet(path, batch_size):
+    transform_train = transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    transform_val = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    
+    train_set = datasets.ImageFolder(root=os.path.join(path, "train"), transform=transform_train)
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    val_set = datasets.ImageFolder(root=os.path.join(path, "val"), transform=transform_val)
+    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
     
     return train_loader, val_loader
