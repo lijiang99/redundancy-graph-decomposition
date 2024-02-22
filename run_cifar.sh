f252a4fd09ce65bfa78b48208edf4956cd67a482
lijiang99 <bd7xzz@163.com>
2024-02-22 17:11:12 +0800

    update shell script

--- run_cifar.sh
+++ run_cifar.sh
@@ -1,5 +1,6 @@
 # !/bin/bash
 archs=("vgg16" "resnet20" "resnet32" "resnet44" "resnet56" "resnet110" "densenet40" "googlenet" "mobilenet_v1" "mobilenet_v2")
+dataset="cifar10"
 
 for arch in ${archs[@]}
 do
@@ -21,16 +22,16 @@ do
 			;;
 		*)
 	esac
-	pretrain_path="./cifar10/pre-train/"$arch"-weights.pth"
+	pretrain_path=$dataset"/pre-train/"$arch"-weights.pth"
 	if [ ! -f "$pretrain_path" ]; then
-		python train_cifar.py --arch $arch
+		python train_cifar.py --arch $arch --dataset $dataset
 	fi
 	for threshold in ${thresholds[@]} 
 	do
-		prune_info_path="./cifar10/prune-info/"$arch"-"$threshold".json"
+		prune_info_path=$dataset"/prune-info/"$arch"-"$threshold".json"
 		if [ ! -f "$prune_info_path" ]; then
-			python generate_prune_info.py --arch $arch --threshold $threshold
+			python generate_prune_info.py --arch $arch --dataset $dataset --threshold $threshold
 		fi
-		python prune_cifar.py --arch $arch --threshold $threshold
+		python prune_cifar.py --arch $arch --dataset $dataset --threshold $threshold
 	done
 done
--- run_imagenet.sh
+++ run_imagenet.sh
@@ -1,9 +1,6 @@
 # !/bin/bash
 archs=("resnet50" "vgg16_bn" "vgg19_bn")
-pretrain_dir="./imagenet/pre-train/"
-dataset_dir="./imagenet/dataset/"
-prune_info_saved_dir="./imagenet/prune-info/"
-prune_info_log_dir="./imagenet/log/generate_prune_info/"
+dataset="imagenet"
 
 for arch in ${archs[@]}
 do
@@ -18,9 +15,9 @@ do
 	esac
 	for threshold in ${thresholds[@]}
 	do
-		prune_info_path="./imagenet/prune-info/"$arch"-"$threshold".json"
+		prune_info_path=$dataset"/prune-info/"$arch"-"$threshold".json"
 		if [ ! -f "$prune_info_path" ]; then
-			python generate_prune_info.py --arch $arch --dataset imagenet --pretrain-dir $pretrain_dir --dataset-dir $dataset_dir --saved-dir $prune_info_saved_dir --log-dir $prune_info_log_dir --threshold $threshold
+			python generate_prune_info.py --arch $arch --dataset $dataset --threshold $threshold
 		fi
 		python prune_imagenet.py --arch $arch --threshold $threshold
 	done
